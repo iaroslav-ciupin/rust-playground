0861b809415a805f3925f626fc2c32f9519e1177
Iaroslav Ciupin <iaroslav.ciupin@ellation.com>
2020-05-14 14:57:32 +0300

    Implement seqential crawler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use std::collections::HashMap;
+mod word_crawler;
+use word_crawler::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+    let crawler = Crawler::new();
+    let url = "https://edition.cnn.com/".to_string();
+    let search = &"covid".to_string();
+    let mut result = crawler.crawl(url, search, 1).await;
+    println!("Word: {} found {} times in following URLs:", search, result.word_count);
+    let mut urls: Vec<String> = result.urls.into_iter().collect();
+    urls.sort();
+    for url in urls {
+        println!("{}", url);
+    }
     Ok(())
 }
--- src/word_crawler.rs
+++ src/word_crawler.rs
@@ -1,2 +1,130 @@
+use linkify::LinkFinder;
+use std::collections::HashSet;
+use std::ops::{Add, AddAssign};
 
-fn
+pub struct Crawler {
+    finder: LinkFinder,
+}
+
+#[derive(Debug, Default)]
+pub struct CrawlResult {
+    pub word_count: usize,
+    pub urls: HashSet<String>,
+}
+
+impl AddAssign for CrawlResult {
+    fn add_assign(&mut self, rhs: Self) {
+        self.word_count += rhs.word_count;
+        for url in rhs.urls {
+            self.urls.insert(url);
+        }
+    }
+}
+
+pub struct CrawlErr {
+    msg: String,
+}
+
+impl Crawler {
+    pub fn new() -> Crawler {
+        Crawler { finder: LinkFinder::new() }
+    }
+
+    fn find_urls(&self, s: &String) -> HashSet<String> {
+        self.finder.links(s.as_str()).map(|i|String::from(i.as_str())).collect()
+    }
+
+    pub async fn crawl(&self, url: String, word: &String, _depth: u8) -> CrawlResult {
+        let mut visited_urls = HashSet::new();
+        let mut to_visit: HashSet<String> = [url].iter().cloned().collect();
+        let mut total: usize = 0;
+        for _ in 1..=1 {
+            let result = self.crawl_urls(&to_visit, word).await;
+            for url in to_visit {
+                visited_urls.insert(url);
+            }
+            total += result.word_count;
+            to_visit = result.urls.into_iter()
+                .filter(|u|!visited_urls.contains(u))
+                .collect();
+        }
+        CrawlResult {
+            word_count: total,
+            urls: visited_urls,
+        }
+    }
+
+    async fn crawl_urls(&self, urls: &HashSet<String>, word: &String) -> CrawlResult {
+        let mut final_result = CrawlResult::default();
+        for url in urls {
+            let result = self.crawl_url(url, word).await;
+            match result {
+                Ok(crawl_result) => {
+                    println!("{}: {} words and {} urls", url, crawl_result.word_count, crawl_result.urls.len());
+                    final_result += crawl_result;
+                },
+                Err(msg) => eprintln!("{}: error - {}", url, msg)
+            }
+        }
+        final_result
+    }
+
+    async fn crawl_url(&self, url: &String, word: &String) -> Result<CrawlResult, String> {
+        let response = reqwest::get(url.as_str()).await.map_err(|e|e.to_string())?;
+        if response.status() != 200 {
+            println!("Bad status: {}", response.status());
+            return Err(format!("status: {}", response.status()))
+        }
+
+        let html: String = response.text().await.map_err(|e|e.to_string())?;
+
+        let word_count = num_occurrences(&html, word);
+        let urls = self.find_urls(&html);
+
+        Ok(CrawlResult{ word_count, urls })
+    }
+}
+
+fn num_occurrences(s: &String, word: &String) -> usize {
+    let to_search = word.trim().to_lowercase();
+    let searchable = s.to_lowercase();
+    searchable.matches(to_search.as_str()).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_num_occurrences() {
+        let s = r#"
+        <html>
+            <body>
+                <h1>Hello</h1>Covid-19... bla-bla how are you Covid-19
+            </body>
+            Covid
+        </html>"#;
+
+        assert_eq!(3, num_occurrences(&s.to_string(), &"Covid".to_string()));
+        assert_eq!(2, num_occurrences(&s.to_string(), &"Covid-19".to_string()));
+    }
+
+    #[test]
+    fn test_find_urls() {
+        let s = r#"
+        <html>
+            <head>Welcome to foo.com</head>
+            <body>
+                <h1>www.bar.com is here</h1>Foo foo foo, com com .com
+            </body>
+            http://example.com
+            https://aws.amazon.com/jora/valerich
+            Covid
+        </html>"#;
+        let expected = vec!["http://example.com", "https://aws.amazon.com/jora/valerich"];
+        let expected: Vec<String> = expected.into_iter().map(String::from).collect();
+        let crawler = Crawler{ finder: LinkFinder::new() };
+
+        assert_eq!(expected, crawler.find_urls(&s.to_string()));
+    }
+}
